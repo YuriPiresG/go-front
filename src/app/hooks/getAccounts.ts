f1c504937afb2216aa87d0572acfcc60b57f4a0b
Yuri Pires <yuripiresbr@gmail.com>
2024-01-02 15:22:49 -0300

    feat: Add getAccounts hook

---
+++ src/app/hooks/getAccounts.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+interface Account {
+  accounts: [
+    {
+      ID: number;
+      CreatedAt: string;
+      UpdatedAt: string;
+      DeletedAt: string;
+      Balance: number;
+      Payments: [];
+    }
+  ];
+}
+
+export async function getAccounts() {
+  const res = await fetch(`http://localhost:3000/account`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const account: Account = await res.json();
+  return account;
+}
