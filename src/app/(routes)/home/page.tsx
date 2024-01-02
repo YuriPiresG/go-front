8348f1258c338bff016adb28f0e6b8f5be8a867d
Yuri Pires <yuripiresbr@gmail.com>
2024-01-02 15:22:40 -0300

    feat: Add home page with AccountsTable component

---
+++ src/app/(routes)/home/page.tsx
@@ -0,0 +1,11 @@
+import AccountsTable from "@/components/AccountsTable";
+
+export default async function page() {
+  return (
+    <>
+      <div className="">
+        <AccountsTable />
+      </div>
+    </>
+  );
+}
